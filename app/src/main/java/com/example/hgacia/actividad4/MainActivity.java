05308ebb62b67895441f3d4c95235bde37b48a41
Hugo Garcia <hhga@hotmail.com>
2017-09-14 14:41:17 -0500

    Primer commit

---
+++ app/src/main/java/com/example/hgacia/actividad4/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.hgacia.actividad4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView iv_weather = (ImageView)findViewById(R.id.iv_weather);
+        int identificador = getResources().getIdentifier("imagen_01n","drawable",getPackageName());
+        iv_weather.setImageDrawable(getResources().getDrawable(identificador,null));
+    }
+}
